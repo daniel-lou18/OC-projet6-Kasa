ee0e6626a7dfce748f25e5ef664ce876b294c02c
daniel-lou18 <danielderudder@hotmail.com>
2024-02-19 09:46:19 +0100

    Corriger padding du container des menus déroulants. Corriger hauteur de l'image en version mobile

--- src/data/about.js
+++ src/data/about.js
@@ -12,7 +12,7 @@ const aboutContent = [
   {
     title: "Service",
     description:
-      "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+      "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
   },
   {
     title: "Sécurité",
